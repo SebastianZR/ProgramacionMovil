6cf2dd6cb49d5ce2bb6858d10f9c2bfccbebf6f5
SebastianZR <sebastianzarraga43@aragon.unam.mx>
2023-02-21 00:03:29 -0600

    Primer Practica con detalles

--- app/src/main/java/fes/aragon/movil/MainActivity.kt
+++ app/src/main/java/fes/aragon/movil/MainActivity.kt
@@ -1,11 +1,41 @@
 package fes.aragon.movil
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toolbar
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import com.google.android.material.navigation.NavigationView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),NavigationView.OnNavigationItemSelectedListener {
+    private lateinit var drawer: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inicioToolsBar()
+    }
+    private fun inicioToolsBar(){
+        val toolbar:Toolbar=findViewById(R.id.toolbar_main)
+        setSupportActionBar(toolbar)
+        drawer=findViewById(R.id.drawer_layout)
+        val toggle=ActionBarDrawerToggle(this, drawer,toolbar,R.string.abrir,R.string.cerrar)
+        drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        toolbar.setNavigationIcon(R.drawable.unam_32)
+        iniciarNavegacionView()
+    }
+    private fun iniciarNavegacionView(){
+        val navegacionView: NavigationView =findViewById(R.id.nav_view)
+        navegacionView.setNavigationItemSelectedListener(this)
+        val headerView: View =LayoutInflater.from(this).inflate(R.layout.header_main,
+            navegacionView,false)
+        navegacionView.addHeaderView(headerView)
+    }
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        drawer.closeDrawer(GravityCompat.START)
+        return true
     }
 }
